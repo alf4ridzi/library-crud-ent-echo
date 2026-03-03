d65f5bbeae73138cf79aa8509ef2c583daa944e6
alf4ridzi <aalalfaridzi9@gmail.com>
2026-03-03 21:57:48 +0700

    add middleware jwt auth

--- internal/delivery/http/routes/routes.go
+++ internal/delivery/http/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
-import "github.com/labstack/echo/v5"
+import (
+	"github.com/alf4ridzi/library-crud-ent-echo/internal/delivery/http/middleware"
+	"github.com/labstack/echo/v5"
+)
 
 type Routes struct {
 	AuthRoute *AuthRoute
@@ -16,6 +19,8 @@ func NewRoutes(
 
 func (r *Routes) Register(router *echo.Echo) {
 	api := router.Group("/api")
+
+	api.Use(middleware.JwtAuth)
 
 	r.AuthRoute.Register(api)
 }
